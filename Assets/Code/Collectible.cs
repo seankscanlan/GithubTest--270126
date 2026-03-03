a368c7b5e7346e037eb81a62310cf5491333b469
seankscanlan <seankscanlan@gmail.com>
2026-03-03 11:05:11 +0000

    Sounds and Effects

--- Assets/Code/Collectible.cs
+++ Assets/Code/Collectible.cs
@@ -12,6 +12,7 @@ public class Collectible : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
+            AudioManager.Instance.PlaySFX(AudioManager.Instance.collectibleSFX);
             GameManager.Instance.AddCollectible();
             Destroy(gameObject);
         }
--- Assets/Code/playerMovement.cs
+++ Assets/Code/playerMovement.cs
@@ -34,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            AudioManager.Instance.PlaySFX(AudioManager.Instance.jumpSFX);
         }
 
         if (spriteRenderer != null)
